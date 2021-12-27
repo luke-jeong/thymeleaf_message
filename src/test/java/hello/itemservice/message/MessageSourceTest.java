f08d14119c77d13ed45dea8fe7164e7917f10440
creatong <jgh3482@gmail.com>
2021-12-27 23:36:07 +0900

    thymeleaf를 이용한 메시지, 국제화

---
+++ src/test/java/hello/itemservice/message/MessageSourceTest.java
@@ -0,0 +1,26 @@
+package hello.itemservice.message;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.MessageSource;
+import org.springframework.context.NoSuchMessageException;
+
+@SpringBootTest
+public class MessageSourceTest {
+    @Autowired
+    MessageSource ms;
+
+    @Test
+    void helloMessage() {
+        String result = ms.getMessage("hello", null, null);
+        Assertions.assertThat(result).isEqualTo("안녕");
+    }
+
+    @Test
+    void notFoundMessageCode(){
+        Assertions.assertThatThrownBy(() -> ms.getMessage("no_code", null, null))
+                .isInstanceOf(NoSuchMessageException.class);
+    }
+}
