a0c4e3da2bef830249ae6a668bf2aa77ca1611a5
creatong <jgh3482@gmail.com>
2021-12-28 22:50:35 +0900

    thymeleaf를 이용한 메시지, 국제화 ko, en 버전 적용 완료

--- src/test/java/hello/itemservice/message/MessageSourceTest.java
+++ src/test/java/hello/itemservice/message/MessageSourceTest.java
@@ -7,6 +7,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.MessageSource;
 import org.springframework.context.NoSuchMessageException;
 
+import java.util.Locale;
+
 @SpringBootTest
 public class MessageSourceTest {
     @Autowired
@@ -22,5 +24,25 @@ public class MessageSourceTest {
     void notFoundMessageCode(){
         Assertions.assertThatThrownBy(() -> ms.getMessage("no_code", null, null))
                 .isInstanceOf(NoSuchMessageException.class);
+    }
+
+    @Test
+    void argumentMessage() {
+        String message = ms.getMessage("hello.name", new Object[]{"Spring"}, null);
+        Assertions.assertThat(message).isEqualTo("안녕 Spring");
+        //기본값인 안녕에 Object에 넣어준 Spring이 들어가 "안녕 Spring" 이 된다.
+    }
+
+    @Test
+    void defaultLang(){
+        Assertions.assertThat(ms.getMessage("hello", null, null)).isEqualTo("안녕");
+        Assertions.assertThat(ms.getMessage("hello", null, Locale.KOREA)).isEqualTo("안녕");
+        //기본값이 한글이기때문에 locale 값이 null일때나 한국일때가 같은 값이 출력된다.
+    }
+
+    @Test
+    void enLang() {
+        Assertions.assertThat(ms.getMessage("hello", null, Locale.ENGLISH)).isEqualTo("hello");
+        //locale 값이 ENGLISH일때 영어로 설정한 값이 나오는지 확인
     }
 }
